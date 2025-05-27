20ee506fec2aa1271e039e6acfe4d176ce475f21
Владислав Головатий <vladyslavgolovatyi@gmail.com>
2025-05-27 18:32:09 +0300

    add more files 2

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ZonesList from './components/ZonesList';
+
+function App() {
+  return (
+      <div className="App">
+        <ZonesList />
+      </div>
+  );
+}
+
+export default App;
