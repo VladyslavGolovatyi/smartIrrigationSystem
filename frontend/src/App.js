d5f53f48c7a401efb927d0fa7b5e51e2fd2c8e5c
Владислав Головатий <vladyslavgolovatyi@gmail.com>
2025-05-30 01:49:52 +0300

    fix issue with links routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ZonesView    from './components/ZonesView';
 import ZoneDetail   from './components/ZoneDetail';
 import SubzoneDetail from "./components/SubzoneDetail";
 
 export default function App() {
     return (
-        <BrowserRouter>
+        <Router>
             <Routes>
                 <Route path="/"          element={<Navigate to="/zones" replace />} />
                 <Route path="/zones"     element={<ZonesView />} />
                 <Route path="/zones/:id" element={<ZoneDetail />} />
                 <Route path="/zones/:zoneId/subzones/:subzoneId" element={<SubzoneDetail />} />
             </Routes>
-        </BrowserRouter>
+        </Router>
     );
 }
